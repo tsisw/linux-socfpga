5576b3e3db614637da2d92730b0fb7dd15580247
Austin Zhang <austin.zhang@intel.com>
2023-10-26 00:50:21 +0800

    HSD#15014517890: firmware: stratix10-svc: Fix segmentation fault during rmmod operation
    Fix segmentation fault during rmmod operation.
    Port changes from https://lore.kernel.org/all/20230623041641.1865437-2-tien.sung.ang@intel.com/#r
    
    Signed-off-by: Adrian Ng Ho Yin <adrian.ho.yin.ng@intel.com>

--- drivers/firmware/stratix10-svc.c
+++ drivers/firmware/stratix10-svc.c
@@ -144,6 +144,7 @@ struct stratix10_svc_data {
  * @complete_status: state for completion
  * @invoke_fn: function to issue secure monitor call or hypervisor call
  * @sdm_lock: only allows a single command single response to SDM
+ * @svc: manages the list of client svc drivers
  *
  * This struct is used to create communication channels for service clients, to
  * handle secure monitor or hypervisor call.
@@ -158,6 +159,7 @@ struct stratix10_svc_controller {
 	struct completion complete_status;
 	svc_invoke_fn *invoke_fn;
 	struct mutex *sdm_lock;
+	struct stratix10_svc *svc;
 };
 
 /**
@@ -1911,6 +1913,7 @@ static int stratix10_svc_drv_probe(struct platform_device *pdev)
 		ret = -ENOMEM;
 		return ret;
 	}
+	controller->svc = svc;
 
 	svc->stratix10_svc_rsu = platform_device_alloc(STRATIX10_RSU, 0);
 	if (!svc->stratix10_svc_rsu) {
@@ -1927,8 +1930,6 @@ static int stratix10_svc_drv_probe(struct platform_device *pdev)
 	if (ret)
 		goto err_put_device;
 
-	dev_set_drvdata(dev, svc);
-
 	pr_info("Intel Service Layer Driver Initialized\n");
 
 	return 0;
@@ -1944,8 +1945,8 @@ err_destroy_pool:
 static void stratix10_svc_drv_remove(struct platform_device *pdev)
 {
 	int i;
-	struct stratix10_svc *svc = dev_get_drvdata(&pdev->dev);
 	struct stratix10_svc_controller *ctrl = platform_get_drvdata(pdev);
+	struct stratix10_svc *svc = ctrl->svc;
 
 	of_platform_depopulate(ctrl->dev);
 
