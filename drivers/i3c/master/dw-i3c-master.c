25ae4c34e5ab7e7fc1f15d8b95e9bd44fbe92b46
Adrian Ng Ho Yin <adrianhoyin.ng@altera.com>
2025-07-03 11:13:43 +0800

    HSD#22021259171: i3c: master: dw-i3c-master: read and configure dev_nack_retry_cntr
    Dev_nack_retry_cntr is required to allow the controller to retry failed
    transactions. This commit is to address the i3c CTS test 1.2.14.
    
    Added implementation to read value to be configured into DAT register
    for dev_nack_retry_cntr bits from device tree property.
    
    Add implementation to configure dev_nack_retry_cntr bits in DAT register
    during i3c device attach and reattach.
    
    Update dw_i3c_master_restore_addrs to read from DAT register before addr
    configuration to prevent overwriting the existing data in the DAT
    register.
    
    Signed-off-by: Adrian Ng Ho Yin <adrianhoyin.ng@altera.com>

--- drivers/i3c/master/dw-i3c-master.c
+++ drivers/i3c/master/dw-i3c-master.c
@@ -203,8 +203,10 @@
 #define EXTENDED_CAPABILITY		0xe8
 #define SLAVE_CONFIG			0xec
 
+#define MAX_DEV_NACK_RETRY_CNT		0x3
 #define DEV_ADDR_TABLE_IBI_MDB		BIT(12)
 #define DEV_ADDR_TABLE_SIR_REJECT	BIT(13)
+#define DEV_ADDR_TABLE_DEV_NACK_RETRY_CNT(x)	(((x) << 29) & GENMASK(30, 29))
 #define DEV_ADDR_TABLE_LEGACY_I2C_DEV	BIT(31)
 #define DEV_ADDR_TABLE_DYNAMIC_ADDR(x)	(((x) << 16) & GENMASK(23, 16))
 #define DEV_ADDR_TABLE_STATIC_ADDR(x)	((x) & GENMASK(6, 0))
@@ -1010,6 +1012,7 @@ static int dw_i3c_master_reattach_i3c_dev(struct i3c_dev_desc *dev,
 	struct i3c_master_controller *m = i3c_dev_get_master(dev);
 	struct dw_i3c_master *master = to_dw_i3c_master(m);
 	int pos;
+	u32 reg;
 
 	pos = dw_i3c_master_get_free_pos(master);
 
@@ -1030,6 +1033,14 @@ static int dw_i3c_master_reattach_i3c_dev(struct i3c_dev_desc *dev,
 	       master->regs +
 	       DEV_ADDR_TABLE_LOC(master->datstartaddr, data->index));
 
+	if (master->dev_nack_retry_cnt) {
+		reg = readl(master->regs +
+			DEV_ADDR_TABLE_LOC(master->datstartaddr, data->index));
+		reg |= DEV_ADDR_TABLE_DEV_NACK_RETRY_CNT(master->dev_nack_retry_cnt);
+		writel(reg, master->regs +
+			DEV_ADDR_TABLE_LOC(master->datstartaddr, data->index));
+	}
+
 	master->devs[data->index].addr = dev->info.dyn_addr;
 
 	return 0;
@@ -1041,6 +1052,7 @@ static int dw_i3c_master_attach_i3c_dev(struct i3c_dev_desc *dev)
 	struct dw_i3c_master *master = to_dw_i3c_master(m);
 	struct dw_i3c_i2c_dev_data *data;
 	int pos;
+	u32 reg;
 
 	pos = dw_i3c_master_get_free_pos(master);
 	if (pos < 0)
@@ -1058,6 +1070,14 @@ static int dw_i3c_master_attach_i3c_dev(struct i3c_dev_desc *dev)
 	writel(DEV_ADDR_TABLE_DYNAMIC_ADDR(master->devs[pos].addr),
 	       master->regs +
 	       DEV_ADDR_TABLE_LOC(master->datstartaddr, data->index));
+
+	if (master->dev_nack_retry_cnt) {
+		reg = readl(master->regs +
+			DEV_ADDR_TABLE_LOC(master->datstartaddr, data->index));
+		reg |= DEV_ADDR_TABLE_DEV_NACK_RETRY_CNT(master->dev_nack_retry_cnt);
+		writel(reg, master->regs +
+			DEV_ADDR_TABLE_LOC(master->datstartaddr, data->index));
+	}
 
 	return 0;
 }
@@ -1629,6 +1649,16 @@ int dw_i3c_common_probe(struct dw_i3c_master *master,
 	if (ret)
 		goto err_disable_pm;
 
+	ret = device_property_read_u32(&pdev->dev, "snps,dev-nack-retry-cnt",
+		&master->dev_nack_retry_cnt);
+	if (ret) {
+		master->dev_nack_retry_cnt = 0;
+	} else if (master->dev_nack_retry_cnt > MAX_DEV_NACK_RETRY_CNT) {
+		pr_info("WARNING dev_nack_retry_cnt is greater than %d!\n", MAX_DEV_NACK_RETRY_CNT);
+		pr_info("Setting dev_nack_retry_cnt to %d!\n", MAX_DEV_NACK_RETRY_CNT);
+		master->dev_nack_retry_cnt = MAX_DEV_NACK_RETRY_CNT;
+	}
+
 	return 0;
 
 err_disable_pm:
@@ -1684,6 +1714,8 @@ static void dw_i3c_master_restore_addrs(struct dw_i3c_master *master)
 	for (pos = 0; pos < master->maxdevs; pos++) {
 		if (master->free_pos & BIT(pos))
 			continue;
+
+		reg_val = readl(master->regs + DEV_ADDR_TABLE_LOC(master->datstartaddr, pos));
 
 		if (master->devs[pos].is_i2c_addr)
 			reg_val = DEV_ADDR_TABLE_LEGACY_I2C_DEV |
