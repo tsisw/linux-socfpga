b01d23e7ac8712251b1965b5e9b18f50796a0389
Niravkumar L Rabara <niravkumarlaxmidas.rabara@altera.com>
2025-09-04 14:12:21 +0800

    HSD#16027672159: EDAC/altera: Use INTTEST register Single-bit error injection
    When ECC RAM is in use by the device controller, injecting errors by
    directly writing/reading ECC RAM can sometimes trigger a false
    double-bit error.
    
    Switch Ethernet and USB EDAC devices to use the INTTEST register
    (altr_edac_a10_device_inject_fops) for single-bit error injection
    instead of flipping bits in ECC RAM directly.
    
    Signed-off-by: Niravkumar L Rabara <niravkumarlaxmidas.rabara@altera.com>

--- drivers/edac/altera_edac.c
+++ drivers/edac/altera_edac.c
@@ -1431,7 +1431,7 @@ static const struct edac_device_prv_data a10_enetecc_data = {
 	.ue_set_mask = ALTR_A10_ECC_TDERRA,
 	.set_err_ofst = ALTR_A10_ECC_INTTEST_OFST,
 	.ecc_irq_handler = altr_edac_a10_ecc_irq,
-	.inject_fops = &altr_edac_a10_device_inject2_fops,
+	.inject_fops = &altr_edac_a10_device_inject_fops,
 };
 
 #endif	/* CONFIG_EDAC_ALTERA_ETHERNET */
@@ -1521,7 +1521,7 @@ static const struct edac_device_prv_data a10_usbecc_data = {
 	.ue_set_mask = ALTR_A10_ECC_TDERRA,
 	.set_err_ofst = ALTR_A10_ECC_INTTEST_OFST,
 	.ecc_irq_handler = altr_edac_a10_ecc_irq,
-	.inject_fops = &altr_edac_a10_device_inject2_fops,
+	.inject_fops = &altr_edac_a10_device_inject_fops,
 };
 
 static int __init socfpga_init_usb3_ecc(struct altr_edac_device_dev *device)
@@ -1542,7 +1542,7 @@ static const struct edac_device_prv_data agilex5_usb3ecc_data = {
 	.ue_set_mask = ALTR_A10_ECC_TDERRA,
 	.set_err_ofst = ALTR_A10_ECC_INTTEST_OFST,
 	.ecc_irq_handler = altr_edac_a10_ecc_irq,
-	.inject_fops = &altr_edac_a10_device_inject2_fops,
+	.inject_fops = &altr_edac_a10_device_inject_fops,
 };
 
 #endif	/* CONFIG_EDAC_ALTERA_USB */
